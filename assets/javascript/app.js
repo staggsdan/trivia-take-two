472556f6668653f195228d7487ebdf0b31d8e25e
Dan Staggs <staggs.dan@gmail.com>
2019-01-26 17:32:15 -0600

    create divs via js/jq, functional answer logic loop, score counter works. timer is buggy. no styling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,11 +5,21 @@ const questions = [{
     answer: 1},
     {question: "Hozac Records was originally a zine called",
     options: ['Honga Zonga','Hot Zesty Action','Horizontal Action','Hog Zed Ace'],
+    answer: 2},
+    {question: "Sympathy for the Record Industry is an independent record label run by : ",
+    options:["Long Gone John", "Strong Stark Ron", "Bold Sizzlin' Tom","Long John Silver"],
+    answer: 0 },
+    {question: "At the time this quiz was written, Billy Childish's discogs profile features how many album and single appearances?",
+    options: ["44", "18", "7", "67"],
+    answer: 3},
+    {question: "The Mono Men once played a live accompaniment to a striptease at a bowling alley in ",
+    options: ["Duboque, IA", "Normal, IL", "Bloomington, IL", "Bloomington, IN"],
     answer: 2}
+
 ];
 
 // global variables
-var timer = 30000;
+var timer;
 var correctTally = 0;
 var incorrectTally = 0;
 var nullAnswerTally = 0;
@@ -17,86 +27,35 @@ var displayedQuestion = "";
 var userChoice = "";
 var questionCounter = 0;
 
-
-
-
-// function to fill the div with a 'start play' card
-// function writeGreeting(){
-//     $('#start-button').hide();
-//     $('#main-div').append('<div id="instructions-div"></div>');
-//     // alert("doin' it");
-    
-//     // $("#main-div").html("<div   id=instructions-div></div>");
-//     $('#instructions-div').text("Do your best to answer all the questions before time runs out!");
-//     $('#main-div').append('<button id="new-button">Lets go</button>');
-// }
-
 function createGameDivs(){
+    
     $('#main-div').append('<div id="question-box"></div>');
-    $('#main-div').append('<div id="answer-zone"></div>')
+    $('#main-div').append('<div id="answer-zone"></div>');
+    $('#main-div').append('<div id="score-zone"></div>');
 }
 
 function loadQuestions(){
+    $('#main-div').prepend('<div id="timer-box"></div>');
+    timer = 30;
+    setInterval(everySecond, 1000);
+    function everySecond() {
+        timer--,
+        $("#timer-box").html("time remaining " + timer);
+      }
     $('#question-box').empty();
     $('#answer-zone').empty();
+    
     $('#question-box').html(questions[questionCounter].question + '<br>');
     for (let i = 0; i < questions[questionCounter].options.length; i++){
         $('#answer-zone').append(`<button id="answer-${i}" class="answer-buttons" value=${i}>${questions[questionCounter].options[i]} </button><br> `);
     }
+    // if (timer < 1) {
+    //     incorrectTally++;
+    //     loadQuestions()
+    // }
+    
 }
 
-
-
-// function to load the questions. 
-// function loadQuestions(){
-//     $('#main-div').append('<div id="question-box"></div>')
-//     $('#question-box').append(questions[questionCounter].question + '<br>');
-//     $('#main-div').append('<div id="answer-zone"></div>')
-//     // $('#answer-zone').append(questions[questionCounter].options)
-    
-//     for (let i = 0; i < questions[questionCounter].options.length; i++) {
-//         // $('#answer-zone').append('<p >' + questions[questionCounter].options[i] + '</p>');
-//         // $('#answer-zone').append('<id="answer-'i'">'questions[questionCounter].options[i] + '<br>')
-//         $('#answer-zone').append(`<button id="answer-${i}" class="answer-buttons" value=${i}>${questions[questionCounter].options[i]} </button><br> `);
-//     };
-//     var currentAnswer = (questions[questionCounter].answer);
-// }
-
-// function to create gameplay divs that leads to function to load questions
-
-// function to compare user input with correct answer
-// function compareInputs(){
-//     for (let i = 0; i < array.length; i++) {
-//         if (questions[i].answer === userChoice[i]) {
-            
-//         }
-        
-//     }
-// }
-
-
-// function to calculate a final scorecard
-
-
-// function to reset counters and allow for a new game
-
-
-// event listener for click to start game
-
-
-// event listener for answer selection
-// document.getElementById("button").addEventListener("click", writeGreeting());
-// writeGreeting();
-
-// $("#start-button").click(function(){
-//     writeGreeting()
-// });
-// document.getElementsByClassName("answer-buttons").addEventListener("click", alert("clicks"));
-
-// $(".answer-buttons").click(function(){
-//     // loadQuestions()
-//     alert("pressed it!");
-// })
 $("#start-button").click(function(){
     $('#start-button').hide();
     createGameDivs();
@@ -104,19 +63,14 @@ $("#start-button").click(function(){
 });
 
 $(document).on('click','.answer-buttons',function(){
-   alert("button value is " + this.value);
-   alert("answer value is " +questions[questionCounter].answer);
-
-   if (this.value === questions[questionCounter].answer) {
+    $('#timer-box').remove();
+   if (this.value == questions[questionCounter].answer) {
        alert("nice");
-       
-//    }
-//     else if (this.value == 2 && questions[1]) {
-//        alert("nice");
-       
-   } else {
-       alert("lame")
+       correctTally++;
+   } else {   
+       incorrectTally++;
    };
+   $('#score-zone').html("current score is " + correctTally + " of 5");
    questionCounter++;
    loadQuestions();
 })
